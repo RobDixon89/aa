bc8281edfe264aabb377191b71f20d75d513892f
RobDixon89 <ginga.rob.dixon@gmail.com>
2025-02-04 11:25:03 +0000

    Add dotenv

---
+++ env.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+
+export const projectId = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID'
+);
+
+export const dataset = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_DATASET,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_DATASET'
+);
+
+export const apiVersion = assertValue(
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  'Missing environment variable: NEXT_PUBLIC_SANITY_API_VERSION'
+);
+
+function assertValue<T>(v: T | undefined, errorMessage: string): T {
+  if (v === undefined) {
+    throw new Error(errorMessage);
+  }
+
+  return v;
+}
